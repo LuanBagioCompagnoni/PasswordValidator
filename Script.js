896ded6388b4fbdf9302440f85e63a6552cd0d52
Luan Bagio Compagnoni <luan_compagnoni@estudante.sc.senai.br>
2023-06-21 20:19:01 -0300

    Adjust all of things

---
+++ Script.js
@@ -0,0 +1,35 @@
+document.getElementById("password").addEventListener("input", passwordValidate);
+let passwords = [];
+function passwordValidate(e) {
+    const a = e.target.value;
+    passwords.push(password);
+    var Number = hasNumber(a);
+    var UpperCase = hasUpperCase(a);
+    var LowerCase = hasLowerCase(a);
+    var TheMinimumValue = hasTheMinimumValue(a);
+    var SpecialsChars = hasSpecialsChars(a);
+    var newPassword = isNewPassword(a);
+    var test = Number && UpperCase && LowerCase && TheMinimumValue && SpecialsChars & newPassword;
+    if (test)
+        document.getElementById('resultado').innerText = "Senha valida!"
+    else
+        document.getElementById('resultado').innerText = "Senha invalida!"
+}
+function hasNumber(password) {
+    return /\d/.test(password);
+}
+function hasUpperCase(password) {
+    return /[A-Z]/.test(password);
+}
+function hasLowerCase(password) {
+    return /[a-z]/.test(password);
+}
+function hasTheMinimumValue(password) {
+    return password.length >= 10;
+}
+function hasSpecialsChars(password) {
+    return /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password);
+}
+function isNewPassword(password) {
+    return !passwords.includes(password);
+}
