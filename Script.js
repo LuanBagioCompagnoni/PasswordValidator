0985a1c8ddc47db7d5be9dd3f7bf023bdb9428da
Luan Bagio Compagnoni <luan_compagnoni@estudante.sc.senai.br>
2023-07-03 13:09:53 -0300

    added visual validation

--- Script.js
+++ Script.js
@@ -1,8 +1,14 @@
 document.getElementById("password").addEventListener("input", passwordValidate);
 let passwords = [];
-function savePassword(e){
+let isValid = false;
+function savePassword(e) {
     const a = document.getElementById('password').value;
-    passwords.push(a);
+    if (isNewPassword(a) && isValid)
+        passwords.push(a);
+    else if (!isNewPassword(a))
+        document.getElementById('resultado').innerText = "Essa senha já existe"
+    else if (!isValid)
+        document.getElementById('resultado').innerText = "Essa senha não é valida"
 }
 function passwordValidate(e) {
     const a = e.target.value;
@@ -11,29 +17,45 @@ function passwordValidate(e) {
     var LowerCase = hasLowerCase(a);
     var TheMinimumValue = hasTheMinimumValue(a);
     var SpecialsChars = hasSpecialsChars(a);
-    var newPassword = isNewPassword(a);
-    var test = Number && UpperCase && LowerCase && TheMinimumValue && SpecialsChars & newPassword;
+    var test = Number && UpperCase && LowerCase && TheMinimumValue && SpecialsChars;
+    isValid = test;
     console.log(passwords)
     if (test)
         document.getElementById('resultado').innerText = "Senha valida!"
-    else
-        document.getElementById('resultado').innerText = "Senha invalida!"
 }
 
 function hasNumber(password) {
+    if (/\d/.test(password))
+        document.getElementById("num").style.color = "green";
+    else
+        document.getElementById("num").style.color = "red";
     return /\d/.test(password);
 }
 function hasUpperCase(password) {
+    if (/[A-Z]/.test(password))
+        document.getElementById("mai").style.color = "green";
+    else
+        document.getElementById("mai").style.color = "red";
     return /[A-Z]/.test(password);
 }
 function hasLowerCase(password) {
-    return /[a-z]/.test(password);
+    if (/[a-z]/.test(password))
+        document.getElementById("minu").style.color = "green";
+    else
+        document.getElementById("minu").style.color = "red";
+    return /[a-z]/.test(password)
 }
 function hasTheMinimumValue(password) {
-    return password.length >= 10;
+    if (password.length >= 10)
+        document.getElementById("min").style.color = "green";
+    else
+        document.getElementById("min").style.color = "red";
 }
 function hasSpecialsChars(password) {
-    return /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password);
+    if (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password))
+        document.getElementById("esp").style.color = "green";
+    else
+        document.getElementById("esp").style.color = "red";
 }
 function isNewPassword(password) {
     return !passwords.includes(password);
